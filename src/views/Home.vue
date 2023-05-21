b79cd89f1cb9f42a3b2673a46b771e3901f37c77
noob2 <v.jord@outlook.com>
2023-05-21 16:30:19 +0300

    added description

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,19 @@
 <template>
   <div class="home">
-    <h2>This is Vue JS app for interview, demonstrating my skills</h2>
+    <v-card variant="outlined">
+      <v-card-text
+        >This app showcases the capabilities of Vue.js and demonstrates my skills as a developer. It utilizes various Vue.js features and best
+        practices to provide a seamless user experience. The app allows users to explore captivating pictures of the day sourced from NASA's API. Upon
+        launching the app, users are presented with a user-friendly interface that includes a date picker. They can select a specific date to retrieve
+        the corresponding 'Picture of the Day' from NASA's API. The app then fetches the image, along with its title and explanation, and displays
+        them in an elegant card layout. To enhance the app's functionality, users can log in to their accounts. Once logged in, they gain access to
+        additional features. One notable feature is the ability to add a picture to their favorites. Users can simply click the heart icon displayed
+        alongside each picture to add it to their favorites collection. The app seamlessly integrates with Vuex, a state management pattern and
+        library for Vue.js, to handle data flow and manage the application's state. Vuex ensures that user information, favorites, and other relevant
+        data are efficiently stored and accessible throughout the app. Overall, this Vue.js app serves as a showcase of my skills in Vue.js
+        development, including Vue components, API integration, state management, and user authentication. It combines functionality, usability, and
+        visual appeal to create an engaging and enjoyable user experience.
+      </v-card-text>
+    </v-card>
   </div>
 </template>
